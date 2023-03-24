c3ae8038954f63389afd89a8f043a1b6527e9d30
LucaPersini <luca.persini@outlook.com>
2023-03-24 18:14:33 +0100

    second upload

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,9 +1,34 @@
-const counter = document.querySelector('.counter')
-const plusButton = document.querySelector('.plus')
-const minusButton = document.querySelector('.minus')
-const resetButton = document.querySelector('.reset')
+const container = document.querySelector('.container')
 
 let value = 0
+
+const counter = document.createElement('div')
+counter.setAttribute('class', 'counter')
+counter.innerHTML = 0
+container.append(counter)
+
+const buttonContainer = document.createElement('div')
+buttonContainer.setAttribute('class', 'buttons')
+container.append(buttonContainer)
+
+const minusButton = document.createElement('button')
+minusButton.setAttribute('class', 'minus')
+minusButton.innerHTML = '-'
+buttonContainer.append(minusButton)
+
+const plusButton = document.createElement('button')
+plusButton.setAttribute('class', 'plus')
+plusButton.innerHTML = '+'
+buttonContainer.append(plusButton)
+
+const resetButtonContainer = document.createElement('div')
+resetButtonContainer.setAttribute('class', 'reset-button')
+container.append(resetButtonContainer)
+
+const resetButton = document.createElement('button')
+resetButton.setAttribute('class', 'reset')
+resetButton.innerHTML = 'Reset'
+resetButtonContainer.append(resetButton)
 
 plusButton.addEventListener('click', () => {
   value++
